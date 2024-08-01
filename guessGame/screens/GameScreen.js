cf5eb58363d8566d703f66345614678886138853
arsouavidzba <avidzba.arsou1@gmail.com>
2024-08-02 01:33:26 +0300

    Initial commit

---
+++ guessGame/screens/GameScreen.js
@@ -0,0 +1,26 @@
+import { Text, View, StyleSheet } from "react-native";
+
+import Title from "../components/Title";
+
+function GameScreen() {
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's guess</Title>
+      {/* Guess */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+        {/* <View>LOG ROUNDS</View> */}
+      </View>
+    </View>
+  );
+}
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
